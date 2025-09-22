9e0be44a05717d08eda07a395765916aafcd3f09
Crabiklol <kirakosyan1320@bk.ru>
2025-09-22 12:35:18 +0300

    Update .cpp

--- .cpp
+++ .cpp
@@ -6,8 +6,7 @@ void n_1() {
     // Целочисленное деление
     int result_int = a / b;
     std::cout << "celochislenni result: " << result_int << std::endl; // Выведет: 3
-
-    double result_double = static_cast<double>(a) / b; 
+	double result_double = static_cast<double>(a) / b; 
     std::cout << "drobni result: " << result_double << std::endl; // Выведет: 3.5
 
 }
@@ -58,17 +57,44 @@ void n_6(){
 	std::cout << "MAX OCENKA: " << MAX_SCORE << std::endl;
 	//MAX_SCORE = 3000;
 	std::cout << "POPITKA IZMENENIA MAX_SCORE na 3000." << std::endl;
+}
+
+ void n_7() {
+   char symbol = 'A';
+   int index = symbol;
+   std::cout << "index A in ASCII: " << index << std::endl;
+    }
+
+
+void n_8(){
+    double num;
+    std::cout << "Paste drobnoe number: ";
+    std::cin >> num;
+    int intNum = static_cast<int>(num);
+    std::cout << "celaya chasth: " << intNum << std::endl;
+}
+
+
+void n_9(){
+    const int numerator = 100;
+    const int denominator = 3;
+
+    int intResult = numerator / denominator;
+    std::cout << "Rezultat Delenuya 100 na 3 kak int: " << intResult << std::endl;
+    double doubleResult = static_cast<double>(numerator) / denominator;
+    std::cout << "rezultat Delenuya 100 na 3 kak double: " << doubleResult << std::endl;
+}
+
+void n_10(){
+    int num;
+     std::cout << "VVedite drobnoe chislo: ";
+     double inputNum;
+     std::cin >> inputNum;
+     num = static_cast<int>(inputNum);
+     std::cout << "Celaya chast chisla: " << num << std::endl;
 
 }
 
-void n_7() {
-	char sym_a = 65;
-	sym_a += 10;
-
-	if (sym_a >= 65 && sym_a <=90){
-		std::cout << "sym_a = " << sym_a <<std::endl;
-	}
-}
 
 int main() {
 	n_1();
@@ -77,5 +103,9 @@ int main() {
 	n_4();
 	n_5();
 	n_6();
+	n_7();
+	n_8();
+	n_9();
+	n_10();
 return 0;
 }
