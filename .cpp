2d56f9ba58cd2dbe0d0f12f4c5446f7e24082750
Crabiklol <kirakosyan1320@bk.ru>
2025-09-22 10:36:48 +0300

    Update .cpp

--- .cpp
+++ .cpp
@@ -5,10 +5,10 @@ void n_1() {
 
     // Целочисленное деление
     int result_int = a / b;
-    std::cout << "Целочисленный результат: " << result_int << std::endl; // Выведет: 3
+    std::cout << "celochislenni result: " << result_int << std::endl; // Выведет: 3
 
     double result_double = static_cast<double>(a) / b; 
-    std::cout << "Дробный результат: " << result_double << std::endl; // Выведет: 3.5
+    std::cout << "drobni result: " << result_double << std::endl; // Выведет: 3.5
 
 }
 
@@ -27,18 +27,51 @@ void n_3() {
 
 
 void n_4() {
-	
+	int grade;
+	std::cout << " YOUR OCENKA ( celoe chislo ot 4 do 5): ";
+	std::cin >> grade ;
+
+	char gradeChar = '0' + grade;
+
+	std::cout << "YOUR OCENKA: " << gradeChar << std::endl;
 }
 
 
+void n_5() {
+	int num1, num2;
+
+std::cout << ": ";
+std::cin >> num1;
+
+std::cout << "paste second celoe chislo: ";
+std::cin >> num2;
+
+double average = static_cast<double>(num1 + num2) / 2;
+
+std::cout << "srednee arifmet : " << average << std::endl;
 
 
+}
 
+void n_6(){
+
+}
+
+void n_7() {
+	char sym_a = 65;
+	sym_a += 10;
+
+	if (sym_a >= 65 && sym_a <=90){
+		std::cout << "sym_a = " << sym_a <<std::endl;
+	}
+}
 
 int main() {
-	void n_1();
-	void n_2();
-	void n_3();
+	n_1();
+	n_2();
+	n_3();
+	n_4();
+	n_5();
 
 return 0;
 }
